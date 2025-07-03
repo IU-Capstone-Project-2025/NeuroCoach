68e01f24dec39fa12081b1c22b317ff29604d552
blazz1t <BlazZ1t>
2025-07-03 03:39:26 +0300

    Fix email not displaying properly on settings screen

--- apps/android-app/lib/features/login/presentation/auth_page.dart
+++ apps/android-app/lib/features/login/presentation/auth_page.dart
@@ -1,9 +1,9 @@
 import 'package:android_app/app/app_router.dart';
+import 'package:android_app/app/presentation/scopes/app_config_scope_container.dart';
 import 'package:android_app/app/presentation/scopes/dependencies_scope.dart';
 import 'package:android_app/features/login/domain/bloc/sign_up_bloc.dart';
 import 'package:android_app/features/login/presentation/components/login_page_contents.dart';
 import 'package:auto_route/auto_route.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -37,6 +37,7 @@ class AuthPage extends StatelessWidget {
           BlocListener<LoginBloc, LoginState>(
             listener: (context, state) {
               if (state is LoginStateLoaded) {
+                AppConfigScopeContainer.of(context).updateEmail(state.email);
                 context.router.replace(HomeRoute());
               } else if (state is LoginStateError) {
                 ScaffoldMessenger.of(context).showSnackBar(
